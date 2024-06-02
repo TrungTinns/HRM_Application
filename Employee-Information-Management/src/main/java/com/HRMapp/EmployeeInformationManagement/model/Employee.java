4a78361b379625ac9395d8d5b42a8d1b09082547
TrungTinns <trungtin073@gmail.com>
2024-06-02 16:33:49 +0700

    Create model E

---
+++ Employee-Information-Management/src/main/java/com/HRMapp/EmployeeInformationManagement/model/Employee.java
@@ -0,0 +1,9 @@
+package com.HRMapp.EmployeeInformationManagement.model;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
+public class Employee {
+    private String id;
+    private String name;
+    private String address; 
+}
