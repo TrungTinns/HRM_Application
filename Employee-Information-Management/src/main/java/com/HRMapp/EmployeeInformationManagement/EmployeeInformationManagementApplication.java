8b30b629af2002fdf9c1e3b71291df4ca2e0a542
TrungTinns <trungtin073@gmail.com>
2024-06-02 16:06:55 +0700

    create EIM

---
+++ Employee-Information-Management/src/main/java/com/HRMapp/EmployeeInformationManagement/EmployeeInformationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.HRMapp.EmployeeInformationManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeInformationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeInformationManagementApplication.class, args);
+	}
+
+}
