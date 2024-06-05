2d31833ac50300c28b02a63ec1c0c263e52115ca
TrungTinns <trungtin073@gmail.com>
2024-06-05 13:48:41 +0700

    create main

---
+++ frontend/hrm_application/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:hrm_application/views/employee_inf_manage/employees.dart';
+import 'package:hrm_application/views/home/home.dart';
+import 'package:flutter/material.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // home: SignIn(),
+      home: Home(),
+    );
+  }
+}
