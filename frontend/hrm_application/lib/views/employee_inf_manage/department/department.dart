954f5ecfdc03403d8df213abac1aeb04a9464fec
TrungTinns <trungtin073@gmail.com>
2024-06-13 12:43:40 +0700

    create form for department

---
+++ frontend/hrm_application/lib/views/employee_inf_manage/department/department.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hrm_application/components/appbar/custom_title_appbar.dart';
+import 'package:hrm_application/components/configuration/configurtion.dart';
+import 'package:hrm_application/components/filter_search/filter_search.dart';
+import 'package:hrm_application/components/search/searchBox.dart';
+import 'package:hrm_application/views/employee_inf_manage/contract/contracts.dart';
+import 'package:hrm_application/views/employee_inf_manage/employee/employees.dart';
+import 'package:hrm_application/views/home/home.dart';
+import 'package:hrm_application/widgets/colors.dart';
+
+class Department extends StatefulWidget {
+  @override
+  _DepartmentState createState() => _DepartmentState();
+}
+
+class _DepartmentState extends State<Department> {
+String pageName = 'Department';
+  bool _isHovered = false;
+  bool _isSidebarOpen = true;
+  bool showEmployeeForm = false;
+  String? activeDropdown;
+  final TextEditingController nameController = TextEditingController();
+
+  void setActiveDropdown(String? dropdown) {
+    setState(() {
+      activeDropdown = dropdown;
+    });
+  }
+
+  void toggleEmployeeForm() {
+    if (showEmployeeForm) {
+      if (nameController.text.isEmpty) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Incomplete Form'),
+              content: Text('Name field is missing.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        setState(() {
+          showEmployeeForm = false;
+          nameController.clear();
+        });
+      }
+    } else {
+      setState(() {
+        showEmployeeForm = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: CustomTitleAppbar(
+          ctx: context,
+          service: pageName,
+          titles: ['Employees', 'Reporting'],
+          options: [
+            ['Employees', 'Department', 'Contracts'],
+            ['Contracts', 'Skills']
+          ],
+          optionNavigations: [
+            [
+              () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => EmployeeManage())),
+              () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Department())),
+              () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Contracts())),
+            ],
+            [
+              () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+              () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+            ],
+          ],
+          activeDropdowns: ['Employees', 'Reporting'],
+          setActiveDropdown: (dropdown) {
+            setState(() {
+              activeDropdown = dropdown;
+            });
+          },
+          config: configuration(
+            isActive: activeDropdown == 'Configuration',
+            onOpen: () => setActiveDropdown('Configuration'),
+            onClose: () => setActiveDropdown(null),
+            titles: ['Setting', 'Employee', 'Recruitment'],
+            options: [
+              ['Setting', 'Activity Plan'],
+              ['Departments', 'Work Locations', 'Working Schedules', 'Departure Reasons', 'Skill Types'],
+              ['Job Positions', 'Employment Types']
+            ],
+            navigators: [
+              [
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+              ],
+              [
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+              ],
+              [
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+                () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
+              ],
+            ],
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    toggleEmployeeForm();
+                  },
+                  child: Text('New', style: TextStyle(color: Colors.white, fontSize: 16)),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      pageName,
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.upload),
+                      color: Colors.white,
+                      onPressed: () {},
+                      tooltip: "Import records",
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              if (!showEmployeeForm)
+                searchBoxWithFilterTable(context, 'Search...', filter(
+                  titles: ['Filter', 'Group By', 'Favorites'],
+                  icons: [Icons.filter_alt, Icons.groups, Icons.star_rounded],
+                  iconColors: [primaryColor, Colors.greenAccent, Colors.yellow],
+                  options: [
+                    ['My Team', 'My Department', 'Newly Hired', 'Achieved'],
+                    ['Manager', 'Department', 'Job', 'Skill', 'Start Date', 'Tags'],
+                    ['Save Current Search']
+                  ],
+                  navigators: [
+                    [
+                      () => Navigator.pushNamed(context, '/my_team'), 
+                      () => Navigator.pushNamed(context, '/my_department'), 
+                      () => Navigator.pushNamed(context, '/newly_hired'), 
+                      () => Navigator.pushNamed(context, '/achieved')],
+                    [
+                      () => Navigator.pushNamed(context, '/manager'), 
+                      () => Navigator.pushNamed(context, '/department'), 
+                      () => Navigator.pushNamed(context, '/job'), 
+                      () => Navigator.pushNamed(context, '/skill'), 
+                      () => Navigator.pushNamed(context, '/start_date'), 
+                      () => Navigator.pushNamed(context, '/tags')],
+                    [() => print('Save Current Search')],
+                  ],
+                )
+              ),
+              if (showEmployeeForm)
+                IconButton(
+                  icon: Icon(Icons.person),
+                  color: Colors.white,
+                  iconSize: 30,
+                  onPressed: () {
+                    // Perform an action
+                  },
+                ),
+              Spacer(),
+            ],
+          ),
+        ),
+        backgroundColor: snackBarColor,
+      ),
+      body: Row(
+      ),
+    );
+  }
+}
---
+++ frontend/hrm_application/lib/views/employee_inf_manage/department/form/department_form.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:hrm_application/widgets/colors.dart';
+
+class DepartmentForm extends StatefulWidget {
+  @override
+  _DepartmentFormState createState() => _DepartmentFormState();
+}
+
+class _DepartmentFormState extends State<DepartmentForm> with SingleTickerProviderStateMixin {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController jobPositionController = TextEditingController();
+  TextEditingController workMobileController = TextEditingController();
+  TextEditingController workPhoneController = TextEditingController();
+  TextEditingController workEmailController = TextEditingController();
+  TextEditingController departmentController = TextEditingController();
+  TextEditingController positionController = TextEditingController();
+  TextEditingController managerController = TextEditingController();
+
+  TabController? tabController;
+
+  bool _isSidebarOpen = true;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    jobPositionController.dispose();
+    workMobileController.dispose();
+    workPhoneController.dispose();
+    workEmailController.dispose();
+    departmentController.dispose();
+    positionController.dispose();
+    managerController.dispose();
+    tabController?.dispose();
+    super.dispose();
+  }
+
+  Future<void> _selectImage() async {
+    
+  }
+
+  Widget buildTextFieldRow(String label, TextEditingController controller) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 100,
+          child: Text(
+            label,
+            style: TextStyle(color: textColor),
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            controller: controller,
+            style: TextStyle(color: textColor),
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: snackBarColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildDropdownRow(String label, TextEditingController controller, List<String> items) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 100,
+          child: Text(
+            label,
+            style: TextStyle(color: textColor),
+          ),
+        ),
+        Expanded(
+          child: DropdownButtonFormField<String>(
+            items: items.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value, style: TextStyle(color: Colors.black)),
+              );
+            }).toList(),
+            onChanged: (value) {
+              controller.text = value!;
+            },
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: snackBarColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: snackBarColor,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextField(
+                        controller: nameController,
+                        style: TextStyle(color: textColor, fontSize: 30.0),
+                        decoration: InputDecoration(
+                          hintText: "Contract Reference",
+                          hintStyle: TextStyle(color: termTextColor, fontSize: 30.0), 
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      buildDropdownRow('Employee', departmentController, ['Department 1', 'Department 2', 'Department 3']),
+                      SizedBox(height: 10),
+                      buildDropdownRow('Contract Start Date', departmentController, ['Department 1', 'Department 2', 'Department 3']),
+                      SizedBox(height: 10),
+                      buildDropdownRow('Contract End Date', departmentController, ['Department 1', 'Department 2', 'Department 3']),
+                      SizedBox(height: 10),
+                      buildDropdownRow('Working Schedule', departmentController, ['Department 1', 'Department 2', 'Department 3']),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: Column(
+                    children: [
+                      buildDropdownRow('Salary Structure Type', departmentController, ['Department 1', 'Department 2', 'Department 3']),
+                      SizedBox(height: 10),
+                      buildDropdownRow('Department', departmentController, ['Department 1', 'Department 2', 'Department 3']),
+                      SizedBox(height: 10),
+                      buildDropdownRow('Job Position', departmentController, ['Department 1', 'Department 2', 'Department 3']),
+                      SizedBox(height: 10),
+                      buildDropdownRow('Contract Type', departmentController, ['Department 1', 'Department 2', 'Department 3']),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            TabBar(
+              controller: tabController,
+              tabs: [
+                Tab(text: 'Salary Information'),
+                Tab(text: 'Contract Details'),
+              ],
+            ),
+            Container(
+              height: 200, 
+              child: TabBarView(
+                controller: tabController,
+                children: [
+                  Center(child: Text('Content for Tab 1')),
+                  Center(child: Text('Content for Tab 2')),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
