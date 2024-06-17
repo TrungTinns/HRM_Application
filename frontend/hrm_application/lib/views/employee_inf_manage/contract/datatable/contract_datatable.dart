3e85c55c032333a4940d5344e673e9c11649ca54
TrungTinns <trungtin073@gmail.com>
2024-06-17 23:40:31 +0700

    change datatable of contracts into plutogrid view

--- frontend/hrm_application/lib/views/employee_inf_manage/contract/datatable/contract_datatable.dart
+++ frontend/hrm_application/lib/views/employee_inf_manage/contract/datatable/contract_datatable.dart
@@ -1,283 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:hrm_application/views/employee_inf_manage/contract/contracts_inf.dart';
+import 'package:hrm_application/widgets/colors.dart';
+import 'package:pluto_grid/pluto_grid.dart';
 
-class ContractDataTable extends StatefulWidget {
-  @override
-  _ContractDataTableState createState() => _ContractDataTableState();
-}
-
-class _ContractDataTableState extends State<ContractDataTable> {
-  final List<Map<String, String>> contracts = [
-    {
-      "Employee": "John Doe",
-      "Reference": "REF123",
-      "Department": "Administration",
-      "Position": "Director",
-      "Start Date": "2022-01-01",
-      "End Date": "2023-01-01",
-      "Contract Type": "Permanent",
-      "Schedule": "Full-time",
-      "Status": "Running",
-    },
-    {
-      "Employee": "Alice Johnson",
-      "Reference": "REF124",
-      "Department": "Research & Development",
-      "Position": "Project Manager",
-      "Start Date": "2021-06-15",
-      "End Date": "2022-06-15",
-      "Contract Type": "Temporary",
-      "Schedule": "Part-time",
-      "Status": "Expired",
-    },
-    {
-      "Employee": "Bob Smith",
-      "Reference": "REF125",
-      "Department": "Marketing",
-      "Position": "Coordinator",
-      "Start Date": "2022-03-01",
-      "End Date": "2023-03-01",
-      "Contract Type": "Full-time",
-      "Schedule": "Full-time",
-      "Status": "Cancelled",
-    },
-  ];
-
-  int _rowsPerPage = PaginatedDataTable.defaultRowsPerPage;
-  int? _sortColumnIndex;
-  bool _sortAscending = true;
-  final Set<int> _selectedRows = {};
-
-  void _sort<T>(Comparable<T> Function(Map<String, String> d) getField, int columnIndex, bool ascending) {
-    contracts.sort((a, b) {
-      if (!ascending) {
-        final Map<String, String> c = a;
-        a = b;
-        b = c;
-      }
-      final Comparable<T> aValue = getField(a);
-      final Comparable<T> bValue = getField(b);
-      return Comparable.compare(aValue, bValue);
-    });
-    setState(() {
-      _sortColumnIndex = columnIndex;
-      _sortAscending = ascending;
-    });
-  }
-
-  void _editCell(int rowIndex, String columnName, String newValue) {
-    setState(() {
-      contracts[rowIndex][columnName] = newValue;
-    });
-  }
-
+class ContractDataTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: PaginatedDataTable(
-        showCheckboxColumn: true,
-        rowsPerPage: _rowsPerPage,
-        onRowsPerPageChanged: (value) {
-          setState(() {
-            _rowsPerPage = value!;
-          });
-        },
-        sortColumnIndex: _sortColumnIndex,
-        sortAscending: _sortAscending,
-        columns: [
-          DataColumn(
-            label: Text('Employee', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['Employee']!, columnIndex, ascending),
-          ),
-          DataColumn(
-            label: Text('Reference', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['Reference']!, columnIndex, ascending),
-          ),
-          DataColumn(
-            label: Text('Department', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['Department']!, columnIndex, ascending),
-          ),
-          DataColumn(
-            label: Text('Position', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['Position']!, columnIndex, ascending),
-          ),
-          DataColumn(
-            label: Text('Start Date', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['Start Date']!, columnIndex, ascending),
-          ),
-          DataColumn(
-            label: Text('End Date', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['End Date']!, columnIndex, ascending),
-          ),
-          DataColumn(
-            label: Text('Contract Type', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['Contract Type']!, columnIndex, ascending),
-          ),
-          DataColumn(
-            label: Text('Schedule', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['Schedule']!, columnIndex, ascending),
-          ),
-          DataColumn(
-            label: Text('Status', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            onSort: (columnIndex, ascending) => _sort<String>((d) => d['Status']!, columnIndex, ascending),
-          ),
-        ],
-        source: ContractDataSource(
-          contracts,
-          _selectedRows,
-          (bool value, int index) {
-            setState(() {
-              if (value) {
-                _selectedRows.add(index);
-              } else {
-                _selectedRows.remove(index);
-              }
-            });
-          },
-          _editCell,
+    List<PlutoColumn> columns = [
+      PlutoColumn(
+        title: 'Employee',
+        field: 'Employee',
+        type: PlutoColumnType.select(['John Doe', 'Alice Johnson', 'Bob Smith']),
+      ),
+      PlutoColumn(
+        title: 'Reference',
+        field: 'Reference',
+        type: PlutoColumnType.select(['REF123', 'REF124', 'REF125']),
+      ),
+      PlutoColumn(
+        title: 'Department',
+        field: 'Department',
+        type: PlutoColumnType.select(['Administration', 'Research & Development', 'Marketing']),
+      ),
+      PlutoColumn(
+        title: 'Position',
+        field: 'Position',
+        type: PlutoColumnType.select(['Director', 'Project Manager', 'Coordinator']),
+      ),
+      PlutoColumn(
+        title: 'Start Date',
+        field: 'Start Date',
+        type: PlutoColumnType.date(),
+      ),
+      PlutoColumn(
+        title: 'End Date',
+        field: 'End Date',
+        type: PlutoColumnType.date(),
+      ),
+      PlutoColumn(
+        title: 'Contract Type',
+        field: 'Contract Type',
+        type: PlutoColumnType.select(['Permanent', 'Temporary', 'Full-time', 'Part-time']),
+      ),
+      PlutoColumn(
+        title: 'Schedule',
+        field: 'Schedule',
+        type: PlutoColumnType.select(['Full-time', 'Part-time']),
+      ),
+      PlutoColumn(
+        title: 'Status',
+        field: 'Status',
+        type: PlutoColumnType.select(['Running', 'Expired', 'Cancelled']),
+      ),
+    ];
+    List<PlutoRow> rows = ContractData.getContracts().map((contract) {
+      return PlutoRow(cells: {
+        'Employee': PlutoCell(value: contract['Employee']),
+        'Reference': PlutoCell(value: contract['Reference']),
+        'Department': PlutoCell(value: contract['Department']),
+        'Position': PlutoCell(value: contract['Position']),
+        'Start Date': PlutoCell(value: contract['Start Date']),
+        'End Date': PlutoCell(value: contract['End Date']),
+        'Contract Type': PlutoCell(value: contract['Contract Type']),
+        'Schedule': PlutoCell(value: contract['Schedule']),
+        'Status': PlutoCell(value: contract['Status']),
+      });
+    }).toList();
+
+    return PlutoGrid(
+      columns: columns,
+      rows: rows,
+      onChanged: (PlutoGridOnChangedEvent event) {
+        
+      },
+      configuration: const PlutoGridConfiguration(
+
+        style: PlutoGridStyleConfig(
+          
+          gridBorderColor:textColor,
+          menuBackgroundColor: snackBarColor,
+          iconColor: textColor,
+          cellColorInEditState: snackBarColor,
+          gridBackgroundColor: snackBarColor,
+          cellTextStyle: TextStyle(color: textColor),
+          columnTextStyle: TextStyle(color: textColor),
+          rowColor: dropdownColor,
+          activatedColor: Color.fromRGBO(38, 42, 54, 1),
+          activatedBorderColor: textColor,
         ),
       ),
     );
   }
-}
-
-class ContractDataSource extends DataTableSource {
-  final List<Map<String, String>> contracts;
-  final Set<int> selectedRows;
-  final Function(bool, int) onSelected;
-  final Function(int, String, String) editCell;
-
-  ContractDataSource(this.contracts, this.selectedRows, this.onSelected, this.editCell);
-
-  @override
-  DataRow? getRow(int index) {
-    assert(index >= 0);
-    if (index >= contracts.length) return null;
-    final contract = contracts[index];
-    final isSelected = selectedRows.contains(index);
-
-    // Example dropdown items for Contract Type
-    List<String> contractTypes = ['Permanent', 'Temporary', 'Full-time', 'Part-time'];
-
-    return DataRow.byIndex(
-      index: index,
-      selected: isSelected,
-      onSelectChanged: (value) => onSelected(value!, index),
-      cells: [
-        DataCell(
-          DropdownButtonFormField<String>(
-            value: contract['Employee'],
-            onChanged: (value) => editCell(index, 'Employee', value!),
-            items: ['John Doe', 'Alice Johnson', 'Bob Smith']
-                .map((String value) => DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    ))
-                .toList(),
-          ),
-          showEditIcon: true,
-        ),
-        DataCell(
-          DropdownButtonFormField<String>(
-            value: contract['Reference'],
-            onChanged: (value) => editCell(index, 'Reference', value!),
-            items: ['REF123', 'REF124', 'REF125']
-                .map((String value) => DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    ))
-                .toList(),
-          ),
-          showEditIcon: true,
-        ),
-        DataCell(
-          DropdownButtonFormField<String>(
-            value: contract['Department'],
-            onChanged: (value) => editCell(index, 'Department', value!),
-            items: ['Administration', 'Research & Development', 'Marketing']
-                .map((String value) => DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    ))
-                .toList(),
-          ),
-          showEditIcon: true,
-        ),
-        DataCell(
-          DropdownButtonFormField<String>(
-            value: contract['Position'],
-            onChanged: (value) => editCell(index, 'Position', value!),
-            items: ['Director', 'Project Manager', 'Coordinator']
-                .map((String value) => DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    ))
-                .toList(),
-          ),
-          showEditIcon: true,
-        ),
-        DataCell(
-          TextFormField(
-            initialValue: contract['Start Date'],
-            onChanged: (value) => editCell(index, 'Start Date', value),
-          ),
-          showEditIcon: true,
-        ),
-        DataCell(
-          TextFormField(
-            initialValue: contract['End Date'],
-            onChanged: (value) => editCell(index, 'End Date', value),
-          ),
-          showEditIcon: true,
-        ),
-        DataCell(
-          DropdownButtonFormField<String>(
-            value: contract['Contract Type'],
-            onChanged: (value) => editCell(index, 'Contract Type', value!),
-            items: contractTypes
-                .map((String value) => DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    ))
-                .toList(),
-          ),
-          showEditIcon: true,
-        ),
-        DataCell(
-          DropdownButtonFormField<String>(
-            value: contract['Schedule'],
-            onChanged: (value) => editCell(index, 'Schedule', value!),
-            items: ['Full-time', 'Part-time']
-                .map((String value) => DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    ))
-                .toList(),
-          ),
-          showEditIcon: true,
-        ),
-        DataCell(
-          DropdownButtonFormField<String>(
-            value: contract['Status'],
-            onChanged: (value) => editCell(index, 'Status', value!),
-            items: ['Running', 'Expired', 'Cancelled']
-                .map((String value) => DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    ))
-                .toList(),
-          ),
-          showEditIcon: true,
-        ),
-      ],
-    );
-  }
-
-  @override
-  int get rowCount => contracts.length;
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get selectedRowCount => selectedRows.length;
 }
