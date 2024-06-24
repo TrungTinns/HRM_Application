eec1a191b85e72ab2427b0dca09228b8587ba462
TrungTinns <trungtin073@gmail.com>
2024-06-25 00:17:44 +0700

    create org chart

--- frontend/hrm_application/lib/views/employee_inf_manage/org chart/orgchart.dart
+++ frontend/hrm_application/lib/views/employee_inf_manage/org chart/orgchart.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:hrm_application/components/appbar/custom_title_appbar.dart';
 import 'package:hrm_application/components/configuration/configurtion.dart';
 import 'package:hrm_application/components/filter_search/filter_search.dart';
@@ -7,21 +6,23 @@ import 'package:hrm_application/components/search/searchBox.dart';
 import 'package:hrm_application/views/employee_inf_manage/contract/contracts.dart';
 import 'package:hrm_application/views/employee_inf_manage/department/department.dart';
 import 'package:hrm_application/views/employee_inf_manage/employee/employees.dart';
+import 'package:hrm_application/views/employee_inf_manage/employee/employees_inf.dart';
+import 'package:hrm_application/views/employee_inf_manage/employee/form/employee_form.dart';
 import 'package:hrm_application/views/home/home.dart';
 import 'package:hrm_application/widgets/colors.dart';
+import 'package:org_chart/org_chart.dart';
 
-class OrgChart extends StatefulWidget {
+class OrgChartManage extends StatefulWidget {
   @override
   _OrgChartState createState() => _OrgChartState();
 }
 
-class _OrgChartState extends State<OrgChart> {
-  String pageName = 'OrgChart';
-  bool _isHovered = false;
-  bool _isSidebarOpen = true;
-
+class _OrgChartState extends State<OrgChartManage> {  
+  String pageName = 'Org Chart';
+  bool showEmployeeForm = false;
+  // bool showEmployeeDetail = false; //Show EmployeeDetail
   String? activeDropdown;
-  bool showContractForm = false;
+  final TextEditingController nameController = TextEditingController();
 
   void setActiveDropdown(String? dropdown) {
     setState(() {
@@ -29,11 +30,73 @@ class _OrgChartState extends State<OrgChart> {
     });
   }
 
-  void toggleContractForm() {
+  void toggleEmployeeForm() {
+    if (showEmployeeForm) {
+      if (nameController.text.isEmpty) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Incomplete Form'),
+              content: Text('Information is missing.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        setState(() {
+          nameController.clear();
+        });
+      }
+    } else {
+      setState(() {
+        showEmployeeForm = true;
+      });
+    }
+  }
+
+  void clearEmployeeForm() {
     setState(() {
-      showContractForm = !showContractForm;
+      showEmployeeForm = false;
     });
   }
+
+  void deleteEmployee(String name) {
+    setState(() {
+      employees.removeWhere((employee) => employee.name == name );
+    });
+  }
+
+  final OrgChartController<Map> orgChartController = OrgChartController<Map>(
+    boxSize: const Size(200, 150),
+    items: [
+      {"title": 'CEO', "id": '1', "to": null},
+      {
+        "title": 'HR Manager: John',
+        "id": '2',
+        "to": '1',
+      },
+      {
+        "title": 'HR Officer: Jane',
+        "id": '3',
+        "to": '2',
+      },
+      {
+        "title": 'Project Manager: Zuher',
+        "id": '4',
+        "to": '1',
+      },
+    ],
+    idProvider: (data) => data["id"],
+    toProvider: (data) => data["to"],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +116,7 @@ class _OrgChartState extends State<OrgChart> {
               () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => EmployeeManage())),
               () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Department())),
               () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Contracts())),
-              () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => OrgChart())),
+              () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => OrgChartManage())),
             ],
             [
               () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
@@ -61,8 +124,11 @@ class _OrgChartState extends State<OrgChart> {
             ],
           ],
           activeDropdowns: ['Employees', 'Reporting'],
-          setActiveDropdown: (dropdown) {            
-          }, 
+          setActiveDropdown: (dropdown) {
+            setState(() {
+              activeDropdown = dropdown;
+            });
+          },
           config: configuration(
             isActive: activeDropdown == 'Configuration',
             onOpen: () => setActiveDropdown('Configuration'),
@@ -90,7 +156,7 @@ class _OrgChartState extends State<OrgChart> {
                 () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => Home())),
               ],
             ],
-          )
+          ),
         ),
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(50),
@@ -100,14 +166,14 @@ class _OrgChartState extends State<OrgChart> {
                 padding: EdgeInsets.all(8.0),
                 child: ElevatedButton(
                   onPressed: () {
-                    toggleContractForm();
+                    toggleEmployeeForm();
                   },
                   child: Text('New', style: TextStyle(color: Colors.white, fontSize: 16)),
                   style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white, 
+                    foregroundColor: textColor,
                     backgroundColor: primaryColor,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5), 
+                      borderRadius: BorderRadius.circular(5),
                     ),
                   ),
                 ),
@@ -120,11 +186,28 @@ class _OrgChartState extends State<OrgChart> {
                       pageName,
                       style: TextStyle(fontSize: 16, color: Colors.white),
                     ),
+                    IconButton(
+                      icon: Icon(Icons.upload),
+                      color: Colors.white,
+                      onPressed: () {},
+                      tooltip: "Import records",
+                    ),
+                    if (showEmployeeForm)
+                      IconButton(
+                        icon: Icon(Icons.clear),
+                        color: Colors.white,
+                        iconSize: 24,
+                        tooltip: "Discard all changes",
+                        onPressed: () {
+                          clearEmployeeForm();
+                        },
+                    ),
                   ],
                 ),
               ),
               Spacer(),
-              searchBoxWithFilterTable(context, 'Search...', filter(
+              if (!showEmployeeForm)
+                searchBoxWithFilterTable(context, 'Search...', filter(
                   titles: ['Filter', 'Group By', 'Favorites'],
                   icons: [Icons.filter_alt, Icons.groups, Icons.star_rounded],
                   iconColors: [primaryColor, Colors.greenAccent, Colors.yellow],
@@ -147,17 +230,119 @@ class _OrgChartState extends State<OrgChart> {
                       () => Navigator.pushNamed(context, '/start_date'), 
                       () => Navigator.pushNamed(context, '/tags')],
                     [() => print('Save Current Search')],
-                  ],
-                )
-              ),
+                  ],)
+                ),
               Spacer(),
             ],
           ),
         ),
         backgroundColor: snackBarColor,
       ),
-      body: Row(
-      ),
+      body: showEmployeeForm
+      // &!showEmployeeDetail
+          ? EmployeeForm()
+          : Stack(
+              children: [
+                Container(
+                  child: Scaffold(
+                    backgroundColor: snackBarColor,
+                    body: Stack(
+                      children: [
+                        Center(
+                          child: OrgChart(
+                            linePaint: Paint()
+                              ..color = Colors.white
+                              ..strokeWidth = 5
+                              ..style = PaintingStyle.stroke,
+                            controller: orgChartController,
+                            curve: Curves.linear, 
+                            duration: 500, 
+                            isDraggable: true, 
+                            builder: (details) {
+                                      return Card(
+                                        color: details.beingDragged
+                                            ? Colors.blue
+                                            : details.isOverlapped
+                                                ? Colors.green
+                                                : snackBarColor,
+                                        elevation: 10,
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+                                        child: Center(
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children:[
+                                              const SizedBox(height: 5,),
+                                              CircleAvatar(
+                                                radius: 20,
+                                                backgroundColor: primaryColor,
+                                                child: Text(
+                                                  details.item["title"].isNotEmpty ? details.item["title"][0].toUpperCase() : '',
+                                                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                                                ),
+                                              ),
+                                              Text(details.item["title"], style: const TextStyle(color: textColor),),
+                                              const SizedBox(height: 10,),
+                                              GestureDetector(
+                                                onTap: () {
+                                                  details.hideNodes(!details.nodesHidden);
+                                                },
+                                                child: Container(  
+                                                  padding: const EdgeInsets.all(8.0),
+                                                  color: authThemeColor,
+                                                  child: const Row(
+                                                    mainAxisAlignment: MainAxisAlignment.center,
+                                                    children: [
+                                                      Icon(
+                                                        Icons.arrow_right_outlined,
+                                                        color: textColor,
+                                                      ),
+                                                      Text(
+                                                        "Show/Hide Nodes",
+                                                        style: TextStyle(color: textColor),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  optionsBuilder: (item) {
+                                      return [
+                                        const PopupMenuItem(value: 'promote', child: Text('Promote')),
+                                        const PopupMenuItem(
+                                            value: 'vacate', child: Text('Vacate Position')),
+                                        const PopupMenuItem(value: 'Remove', child: Text('Remove')),
+                                      ];
+                                    },
+                                    onOptionSelect: (item, value) {
+                                      if (value == 'Remove') {
+                                        orgChartController.removeItem(item["id"]);
+                                        setState(() {});
+                                      }
+                                    },
+                                    
+                          ),
+                        ),
+                      ],
+                    ),
+                    floatingActionButton: FloatingActionButton.extended(
+                        label: const Text('Change Orientation'),
+                        onPressed: () async {
+                          orgChartController.orientation =
+                              orgChartController.orientation ==
+                                      OrgChartOrientation.leftToRight
+                                  ? OrgChartOrientation.topToBottom
+                                  : OrgChartOrientation.leftToRight;
+                          orgChartController.calculatePosition();
+                          setState(() {});
+                        }),
+                  ),
+                ),
+              ],
+            )
     );
   }
 }
