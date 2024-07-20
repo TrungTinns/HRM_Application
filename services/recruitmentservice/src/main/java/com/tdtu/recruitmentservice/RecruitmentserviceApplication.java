45ae54f96cba4fececa9afa0e68c659b6e4d1dc7
neit1803 <tien18032002@gmail.com>
2024-07-20 10:45:23 +0700

    Initial Recruitment Service Application

---
+++ services/recruitmentservice/src/main/java/com/tdtu/recruitmentservice/RecruitmentserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tdtu.recruitmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentserviceApplication.class, args);
+	}
+
+}
