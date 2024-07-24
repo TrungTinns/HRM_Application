9043ee8497ed3664e59bfd5d01bed43e075021c1
neit1803 <tien18032002@gmail.com>
2024-07-24 18:46:03 +0700

    Update (EmployeeService: update FirestoreUtil)

--- services/employeeservice/src/main/java/com/tdtu/employeeservice/util/FirestoreUtil.java
+++ services/employeeservice/src/main/java/com/tdtu/employeeservice/util/FirestoreUtil.java
@@ -7,10 +7,23 @@ import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
+import com.tdtu.employeeservice.command.data.contract.Contract;
 
 public class FirestoreUtil {
 	public static DocumentReference getDocumentReference(String documentPath) {
         Firestore firestore = FirestoreClient.getFirestore();
         return firestore.document(documentPath);
-  }
+	}
+	
+	public static Contract getContract(DocumentReference documentReference)
+			throws InterruptedException, ExecutionException {
+		Firestore db = FirestoreClient.getFirestore();
+		ApiFuture<DocumentSnapshot> future = documentReference.get();
+		DocumentSnapshot document = future.get();
+		if (document.exists()) {
+			return document.toObject(Contract.class);
+		} else {
+			throw new RuntimeException("Level document not found for reference: " + documentReference.getPath());
+		}
+	}
 }
