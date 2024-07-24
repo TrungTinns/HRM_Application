62f4bb00b4c16b7961fa08ee567760f6bf10fc43
neit1803 <tien18032002@gmail.com>
2024-07-24 23:47:46 +0700

    Create (RecruitmentService: Create Candidate Model)

---
+++ services/recruitmentservice/src/main/java/com/tdtu/recruitmentservice/command/data/candidate/Candidate.java
@@ -0,0 +1,55 @@
+package com.tdtu.recruitmentservice.command.data.candidate;
+
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import com.google.cloud.firestore.DocumentReference;
+import com.tdtu.employeeservice.command.data.contract.Contract;
+import com.tdtu.employeeservice.util.FirestoreUtil;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Candidate {
+	private String id;
+	private String name;
+	private String role;
+	private String mail;
+	private String mobile;
+	private String department;
+	private String managerId;
+	private boolean isManager;
+	private String workLocation;
+	private String personalAddress;
+	private String personalMail;
+	private String personalMobile;
+	private String relativeName;
+	private String relativeMobile;
+	private String certification;
+	private String school;
+	private String maritalStatus;
+	private int child;
+	private String nationality;
+	private String idNum;
+	private String ssNum;
+	private String passport;
+	private String sex;
+	private Date birthDate;
+	private String birthPlace;
+	private DocumentReference contract;
+	
+	public Contract getContract() throws InterruptedException, ExecutionException {
+		return FirestoreUtil.getContract(contract);
+	}
+	
+	public DocumentReference getContractRef() {
+		return this.contract;
+	}
+	
+}
---
+++ services/recruitmentservice/src/main/java/com/tdtu/recruitmentservice/command/data/candidate/CandidateService.java
@@ -0,0 +1,42 @@
+package com.tdtu.recruitmentservice.command.data.candidate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+@Service
+public class CandidateService {
+
+	@Autowired
+	CandidateRepository repo;
+
+	public String save(Candidate e) throws InterruptedException, ExecutionException {	
+		return repo.save(e);
+	}
+
+	public String update(Candidate e) throws InterruptedException, ExecutionException {
+		return repo.update(e);
+	}
+
+	public String deleteById(String id) {
+		return repo.deleteById(id);
+	}
+
+	public Candidate findById(String id) throws InterruptedException, ExecutionException {
+		return repo.findById(id);
+	}
+
+	public List<Candidate> findAll() throws InterruptedException, ExecutionException {
+		return repo.findAll();
+	}
+}
