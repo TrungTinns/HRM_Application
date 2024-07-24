ecd8674e9f9f9cf7b81545f9017d98726980ae3f
neit1803 <tien18032002@gmail.com>
2024-07-24 23:34:50 +0700

    Create (RecruitmentService: Create AxonConfig)

---
+++ services/recruitmentservice/src/main/java/com/tdtu/recruitmentservice/config/AxonConfig.java
@@ -0,0 +1,20 @@
+package com.tdtu.recruitmentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.thoughtworks.xstream.XStream;
+
+@Configuration
+public class AxonConfig {
+	
+	 @Bean
+	    public XStream xStream() {
+	        XStream xStream = new XStream();
+	        xStream.allowTypesByWildcard(new String[] {
+	            "com.tdtu.employeeservice.**"
+	        });
+	        xStream.registerConverter(new DocumentReferenceConverter());
+	        return xStream;
+	    }
+}
