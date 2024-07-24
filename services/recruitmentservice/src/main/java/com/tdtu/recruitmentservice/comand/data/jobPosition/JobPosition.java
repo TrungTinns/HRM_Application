bda6801eefd22ca0ae76686de6085f902332d987
neit1803 <tien18032002@gmail.com>
2024-07-25 00:02:18 +0700

    Create (RecruitmentService: Create JobPositionModel)

---
+++ services/recruitmentservice/src/main/java/com/tdtu/recruitmentservice/comand/data/jobPosition/JobPosition.java
@@ -0,0 +1,25 @@
+package com.tdtu.recruitmentservice.comand.data.jobPosition;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class JobPosition {
+	private String id;
+	private String name;
+	private String managerId;
+	private String parentDepartmentId;
+	private String department;
+	private String jobLocation;
+	private String mailAlias;
+	private String empType;
+	private int target;
+	private String recruiterId;
+	private String interviewers;
+	private String description;
+}
