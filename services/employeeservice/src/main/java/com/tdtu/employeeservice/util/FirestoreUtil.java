6eecd0d8e4ec8223cccff940744719d57347fd35
neit1803 <tien18032002@gmail.com>
2024-07-24 12:48:30 +0700

    Updated (EmployeeService: FirestoreUtil)

--- services/employeeservice/src/main/java/com/tdtu/employeeservice/util/FirestoreUtil.java
+++ services/employeeservice/src/main/java/com/tdtu/employeeservice/util/FirestoreUtil.java
@@ -7,22 +7,10 @@ import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
-import com.tdtu.employeeservice.command.data.experience.Experience;
-import com.tdtu.employeeservice.command.data.level.Level;
-import com.tdtu.employeeservice.command.data.resume.Resume;
 
 public class FirestoreUtil {
-	public static Resume getResume(DocumentReference documentReference)
-			throws InterruptedException, ExecutionException {
-		Firestore db = FirestoreClient.getFirestore();
-		ApiFuture<DocumentSnapshot> future = documentReference.get();
-		DocumentSnapshot document = future.get();
-		if (document.exists()) {
-			Resume resume = document.toObject(Resume.class);
-			return resume;
-		} else {
-			throw new RuntimeException("Resume document not found for reference: " + documentReference.getPath());
-		}
-	}
-
+	public static DocumentReference getDocumentReference(String documentPath) {
+        Firestore firestore = FirestoreClient.getFirestore();
+        return firestore.document(documentPath);
+  }
 }
