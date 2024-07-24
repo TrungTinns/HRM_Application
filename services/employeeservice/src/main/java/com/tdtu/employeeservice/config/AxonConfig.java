e7841c659f4eec6b55c5342f3a8ef51740b59b6a
neit1803 <tien18032002@gmail.com>
2024-07-24 12:43:28 +0700

    Updated (EmployeeService: AxonConfig)

--- services/employeeservice/src/main/java/com/tdtu/employeeservice/config/AxonConfig.java
+++ services/employeeservice/src/main/java/com/tdtu/employeeservice/config/AxonConfig.java
@@ -3,8 +3,6 @@ package com.tdtu.employeeservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.tdtu.employeeservice.command.data.level.LevelDetailed;
-import com.tdtu.employeeservice.command.event.skill.SkillCreatedEvent;
 import com.tdtu.employeeservice.converter.DocumentReferenceConverter;
 import com.thoughtworks.xstream.XStream;
 
@@ -18,8 +16,6 @@ public class AxonConfig {
             "com.tdtu.employeeservice.**"
         });
         xStream.registerConverter(new DocumentReferenceConverter());
-        xStream.processAnnotations(SkillCreatedEvent.class);
-        xStream.processAnnotations(LevelDetailed.class);
         return xStream;
     }
 }
