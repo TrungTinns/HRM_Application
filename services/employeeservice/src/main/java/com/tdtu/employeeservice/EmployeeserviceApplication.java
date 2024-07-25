dc01d2bccae5e20f3edb7ab449b531ea097bab01
neit1803 <tien18032002@gmail.com>
2024-07-25 15:09:43 +0700

    Update (EmployeeService: Consumer Message)

--- services/employeeservice/src/main/java/com/tdtu/employeeservice/EmployeeserviceApplication.java
+++ services/employeeservice/src/main/java/com/tdtu/employeeservice/EmployeeserviceApplication.java
@@ -4,16 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.tdtu.employeeservice.config.AxonConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableKafka
 @Import({ AxonConfig.class })
+@ComponentScan(basePackages = "com.tdtu.employeeservice")
 public class EmployeeserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeserviceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeserviceApplication.class, args);
+    }
 }
