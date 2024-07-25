b7220bd657578c83e946c68cba527e251eb9338b
neit1803 <tien18032002@gmail.com>
2024-07-25 11:45:06 +0700

    Update (RecruitmentService: Add Kafka dependencies)

--- services/recruitmentservice/src/main/java/com/tdtu/recruitmentservice/RecruitmentserviceApplication.java
+++ services/recruitmentservice/src/main/java/com/tdtu/recruitmentservice/RecruitmentserviceApplication.java
@@ -2,8 +2,14 @@ package com.tdtu.recruitmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
+import com.tdtu.recruitmentservice.config.AxonConfig;
+
+@EnableDiscoveryClient
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class RecruitmentserviceApplication {
 
 	public static void main(String[] args) {
