940927c210b90345ec2ca7f0aaff30f91afccb48
neit1803 <tien18032002@gmail.com>
2024-07-31 12:55:53 +0700

    Create (TimeSheet Serivce: ComplianceRule model)

---
+++ services/timesheetservice/src/main/java/com/tdtu/timesheetservice/command/data/complianceRules/ComplianceRule.java
@@ -0,0 +1,20 @@
+package com.tdtu.timesheetservice.command.data.complianceRules;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ComplianceRule {
+	private String id;
+	private String ruleDescription;
+	private String penaltyDescription;
+	private Double penaltyAmount;
+	private Date effectiveDate;
+}
