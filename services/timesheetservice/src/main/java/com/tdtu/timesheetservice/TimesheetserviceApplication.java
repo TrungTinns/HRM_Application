fcf65914b96c7533bde31e3b1697b8a7a5e1e184
neit1803 <tien18032002@gmail.com>
2024-07-31 10:20:40 +0700

    Create (TimeSheet Service: Initial Project)

---
+++ services/timesheetservice/src/main/java/com/tdtu/timesheetservice/TimesheetserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tdtu.timesheetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetserviceApplication.class, args);
+	}
+
+}
---
+++ services/timesheetservice/src/test/java/com/tdtu/timesheetservice/TimesheetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdtu.timesheetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
