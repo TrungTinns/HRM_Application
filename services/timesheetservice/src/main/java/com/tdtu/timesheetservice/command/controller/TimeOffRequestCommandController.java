5a755518974a60aa710bd6df1c387e2541e53b0e
neit1803 <tien18032002@gmail.com>
2024-08-10 12:04:57 +0700

    Update (TimeSheet Serivce: TimeOffRequest Logic Create/Update)

--- services/timesheetservice/src/main/java/com/tdtu/timesheetservice/command/controller/TimeOffRequestCommandController.java
+++ services/timesheetservice/src/main/java/com/tdtu/timesheetservice/command/controller/TimeOffRequestCommandController.java
@@ -4,18 +4,23 @@ import java.util.UUID;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.tdtu.timesheetservice.command.command.timeOffRequest.CreateTimeOffRequestCommand;
 import com.tdtu.timesheetservice.command.command.timeOffRequest.DeleteTimeOffRequestCommand;
 import com.tdtu.timesheetservice.command.command.timeOffRequest.UpdateTimeOffRequestCommand;
 import com.tdtu.timesheetservice.command.model.TimeOffRequestRequestModel;
+import com.tdtu.timesheetservice.query.model.ErrorResponseModel;
 
 @RestController
 @RequestMapping("/api/v1/timesheet/timeOffRequest")
@@ -25,6 +30,10 @@ public class TimeOffRequestCommandController {
 
     @PostMapping
     public String addTimeOffRequest(@RequestBody TimeOffRequestRequestModel model) {
+    	if (model.getStartDate().after(model.getEndDate())) {
+    		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Start date cannot be after the end date");
+    	}
+    	
         CreateTimeOffRequestCommand command = new CreateTimeOffRequestCommand(
                 UUID.randomUUID().toString(),
                 model.getEmpId(),
@@ -40,6 +49,10 @@ public class TimeOffRequestCommandController {
 
     @PutMapping
     public String updateTimeOffRequest(@RequestBody TimeOffRequestRequestModel model) {
+    	if (model.getStartDate().after(model.getEndDate())) {
+    		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Start date cannot be after the end date");
+    	}
+    	
         UpdateTimeOffRequestCommand command = new UpdateTimeOffRequestCommand(
                 model.getId(),
                 model.getEmpId(),
@@ -58,5 +71,18 @@ public class TimeOffRequestCommandController {
     public String deleteTimeOffRequest(@PathVariable String id) {
         commandGateway.sendAndWait(new DeleteTimeOffRequestCommand(id));
         return "Deleted a TimeOffRequest with ID: " + id;
+    }
+    
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponseModel> handleResponseStatusException(ResponseStatusException e) {
+        String message = e.getReason() != null ? e.getReason() : "An unexpected error occurred";
+        ErrorResponseModel errorResponse = new ErrorResponseModel(e.getStatusCode().value(), message);
+        return new ResponseEntity<>(errorResponse, e.getStatusCode());
+    }
+		
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseModel> handleException(Exception e) {
+    	ErrorResponseModel errorResponse = new ErrorResponseModel(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
